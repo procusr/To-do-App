90ce61c156a613c2c0440be3b17a81342b104873
Faris Ibrahim <procusr@gmail.com>
2019-02-27 08:52:03 +0100

    Update DisplayTodoList.java

--- src/DisplayTodoList.java
+++ src/DisplayTodoList.java
@@ -1,16 +1,37 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class DisplayTodoList {
+    ReadAndWrite rw;
+    private EditTodoList editor ;
 
     public DisplayTodoList(){
+        rw = new ReadAndWrite();
     }
+
+    public void displayMainMenu(){
+        System.out.println("------------------------------------------------");
+        displayDoneAndUndone(rw.readTaskAsAList());
+        System.out.println("------------------------------------------------");
+        System.out.println("1.Add to do list \t\t2.View Your Tasks");
+        System.out.println("3.Remove a Task \t\t4.Edit Task");
+        System.out.println("5.Sort by Date\t\t    6.Group by project");
+        System.out.println("7.Mark Task as done\t\t8.Save and Quit");
+        System.out.println("------------------------------------------------");
+    }
+
+    public void displayDoneAndUndone(List<Task> tasks){
+        int done_tasks=0,undone_taks=0;
+        for(Task task:tasks){
+            if(task.getStatus()==true){
+                done_tasks++;
+            }
+            else{
+                undone_taks++;
+            }
+        }
+        System.out.println("You have " + done_tasks + "  tasks Completed  and  " + undone_taks + "  Tasks to do");
+    }
+
 
     public void readFileAndDisplayList() {
         List<Task> loadedFromFile = new ArrayList<>();
@@ -44,5 +65,12 @@ public class DisplayTodoList {
         List<Task> sorted = tasks.stream().sorted(Comparator.comparing(task->task.getProject())).collect(Collectors.toList());
         displayList(sorted);
     }
+
+    public void sortBytype(List<Task> tasks,String content){
+        List<Task> sorted = tasks.stream().sorted(Comparator.comparing(task->content)).collect(Collectors.toList());
+        displayList(sorted);
+    }
+
+
 }
 
